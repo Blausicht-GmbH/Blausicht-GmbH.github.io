7d124a9e507c61be3e2b6443a3d3814860c3b220
Syalas <kevin.brenzikofer@gmail.com>
2025-04-05 15:28:46 +0200

    modified routing again

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,13 @@
 import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  constructor(private viewportScroller: ViewportScroller) { }
 
-  scrollTo(sectionId: string): void {
-    this.viewportScroller.scrollToAnchor(sectionId);
-  }
 }
